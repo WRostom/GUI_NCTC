a6e81cbc5da2fabf3bedb24f788ea5da9e63bb07
Waleed Rostom <rostomrevel@gmail.com>
2018-01-26 03:03:53 +0200

    Update sendMessage.php

--- html/mqtt/sendMessage.php
+++ html/mqtt/sendMessage.php
@@ -5,8 +5,8 @@ require($root."/mqtt/phpMQTT.php");
 function sendMessage($message = NULL){
     $mqtt = new phpMQTT("192.168.1.14", 1883, "user1");
 
-    if ($mqtt->connect(true, null, "wrostom", "Een22387")) {
-        $mqtt->publish("wrostom/dev/onoff",$message,0);
+    if ($mqtt->connect(true, null, "USERNAME", "PASSWORD")) {
+        $mqtt->publish("MQTT TOPIC",$message,0);
         $mqtt->close();
     }
     return $message;
